48d3b0a6ca96132bb4122fd6258a82a9ba5fb003
Samuel <samuel310597@gmail.com>
2020-06-04 11:13:53 +0200

    Add Todo component and display them

--- src/components/Contenu.vue
+++ src/components/Contenu.vue
@@ -5,26 +5,47 @@
 
         <form>
             <div class="form-group">
-                <label for="action">Action</label>
-                <input type="text" id="action">
+                <label for="action">Ajouter :</label>
+                <input type="text" id="action" class="form-control">
             </div>
+            <button class="btn btn-primary">Add</button>
 
         </form>
-
+        <ul>
+            <li v-bind:key="index" v-for="(toDo, index) in array">
+                <item :toDo="toDo"></item>
+            </li>
+        </ul>
     </div>
+
 </template>
 
 <script>
+import Item from './Item';
 
 export default {
 	name: 'Contenu',
-
+	data() {
+		return {
+			formData: {
+				toDo: '',
+			},
+			array: ['Test', 'Test1', 'Test2'],
+		};
+	},
+	components: {
+		Item: Item,
+	},
 };
 </script>
 
-<style>
+<style scoped>
     h1 {
-        margin-top: 100px!important;
+        margin-top: 100px !important;
+    }
+
+    ul {
+        list-style: none;
     }
 </style>
 
---
+++ src/components/Item.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card my-2 item-todo">
+        <p>{{ toDo }}</p>
+        <div class="btn-supr btn btn-danger">X</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name :'Item',
+    props : [
+        'toDo'
+    ]
+}
+</script>
+
+
+<style scoped>
+
+    .item-todo {
+        position: relative;
+        padding: 20px 60px 20px 30px;
+        font-size: 30px;
+        height: 80px;
+    }
+
+    .btn-supr {
+        text-align: center;
+        vertical-align: middle;
+        width: 80px;
+        height: 50px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+
+</style>
